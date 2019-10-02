5515b29337cee678c375c0cd7899b2b70688db7e
Juno Morrow <sabotaione@gmail.com>
2019-10-02 16:44:11 -0400

    added tag example

---
+++ Assets/Scripts/TagList.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TagList : MonoBehaviour
+{
+
+    public GameObject[] objectTagArray; //store our array of gameobjects... we made it public so we can see it in the inspector
+    public string tagName; //let the developer type the tag in the inspector
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        objectTagArray = GameObject.FindGameObjectsWithTag(tagName); //assign out all the gameobjects with the tag we typed in the inspector
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
