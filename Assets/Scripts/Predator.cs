26f1d753923aab0f581e466870146dda10e20d6d
Juno Morrow <sabotaione@gmail.com>
2019-10-23 16:44:08 -0400

    added a few comments

--- Assets/Scripts/Predator.cs
+++ Assets/Scripts/Predator.cs
@@ -4,19 +4,20 @@ using UnityEngine;
 
 public class Predator : MonoBehaviour
 {
-    public Transform target;
-    Rigidbody rb;
-    public float forceAmt = 10f;
+    public Transform target; //the thing we want to chase
+    Rigidbody rb; //reference for our rigidbody
+    public float forceAmt = 10f; //amount to multiply to the direction to move it
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>(); //assign it to the rigidbody attached to the same game object
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         Vector3 targetDir = Vector3.Normalize(target.position - transform.position);
 
         rb.AddForce(targetDir * forceAmt);
