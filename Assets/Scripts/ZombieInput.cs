f97f535a571c2859bee7828b0dc9fbe69a888fe8
Juno Morrow <sabotaione@gmail.com>
2019-11-06 16:47:35 -0500

    added week 9ish content (animations)

---
+++ Assets/Scripts/ZombieInput.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieInput : MonoBehaviour
+{
+    Animator myAnim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAnim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myAnim.SetInteger("AnimState", -1);
+    }
+}
